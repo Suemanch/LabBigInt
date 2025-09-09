55dfbff7572188d55aab1e2636596e06c96baeef
Sunveray <vlados2016vlados@list.ru>
2025-09-09 22:47:40 +0700

    Update classBigInt.h
    add  func "addition"

--- classBigInt.h
+++ classBigInt.h
@@ -2,7 +2,7 @@
 #ifndef CLASSBIGINT_H
 #define CLASSBIGINT_H
 
-#include <string.h>
+#include <stdlib.h>
 
 class BigInt
 {
@@ -16,7 +16,29 @@ class BigInt
             length = input->length();
         }
 
-    std::string addition(std::string bigInt1, std::string bigInt2);
+        BigInt()
+        {
+            length = 0;
+        }
+
+        std::string addition(std::string string1, std::string string2)
+        {
+            int sum = 0;
+            int resultLength = std::max(string1.length(), string2.length());
+            std::string * result = new std::string[resultLength];
+
+            for (int i = length; i > 0; i--)
+            {
+                sum = atoi(&string1[i]) + atoi(&string2[i]);
+                if (sum > 9)
+                {
+                    result[i] = sum % 10 + '0';
+                    result[i + 1] = sum / 10 + '0';
+                }
+            }
+            return *result;
+            delete [] result;
+        }
 
 };
 
