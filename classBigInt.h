0ba3642fcc3df7e2e99b61db453184fd051f5a32
Sunveray <vlados2016vlados@list.ru>
2025-09-10 02:22:22 +0700

    Update classBigInt.h
    create a working version of addition func

--- classBigInt.h
+++ classBigInt.h
@@ -24,18 +24,52 @@ class BigInt
         std::string addition(std::string string1, std::string string2)
         {
             int sum = 0;
+            char symbol;
             int resultLength = std::max(string1.length(), string2.length());
             std::string * result = new std::string[resultLength];
+            int counter = resultLength;
 
-            for (int i = length; i > 0; i--)
+            while (counter > 0)
             {
-                sum = atoi(&string1[i]) + atoi(&string2[i]);
-                if (sum > 9)
+                if (string1.length() == 0 || string2.length() == 0)
                 {
-                    result[i] = sum % 10 + '0';
-                    result[i + 1] = sum / 10 + '0';
+                    if (string1.length() == 0)
+                    {
+                        *result += atoi(&string2.back()) % 10 + '0';
+                        string2.pop_back();
+                        counter--;
+                    }
+                    else
+                    {
+                        result += atoi(&string1.back()) % 10 + '0';
+                        string1.pop_back();
+                        counter--;
+                    }
+
                 }
+
+                else
+                {
+                    sum = atoi(&string1.back()) + atoi(&string2.back()); // get the last digits
+
+                    if (sum > 9)
+                    {
+                        symbol = sum / 10 + '0';
+                        result->insert(counter - 1, &symbol);
+                    }
+
+                    symbol = sum % 10 + '0';
+                    *result += symbol;
+                    counter--;
+
+                    string1.pop_back(); // throw away last symbols - we counted them
+                    string2.pop_back();
+                    sum = 0;
+                }
+
             }
+
+            std::reverse(result->begin(), result->end());
             return *result;
             delete [] result;
         }
