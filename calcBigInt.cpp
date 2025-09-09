d6c1b8d32f68fea614efcc75467800e9b47ae560
Sunveray <vlados2016vlados@list.ru>
2025-09-09 21:20:59 +0700

    Create calcBigInt.cpp

---
+++ calcBigInt.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "classBigInt.h"
+
+std::string addition(std::string bigInt1, std::string bigInt2)
+{
+    if (bigInt1[0] == '-' || bigInt2[0] == '-')
+    {
+        bigInt1 = "-" + bigInt1;
+    }
+
+}
+
+
+
+
