6c332b39db1b53e99a9b21057a787f7ca069b361
Sunveray <vlados2016vlados@list.ru>
2025-09-17 12:58:04 +0700

    Update main.cpp
    did ++, -, +

--- main.cpp
+++ main.cpp
@@ -6,24 +6,25 @@
 
 int main()
 {
-    auto * operation = new std::string;
+    // auto * operation = new std::string;
 
     std::string stringBigInt1;
     std::string stringBigInt2;
 
-    std::cin >> stringBigInt1 >> *operation >> stringBigInt2;
+    std::cin >> stringBigInt1  >> stringBigInt2;
 
     BigInt bigInt1(stringBigInt1);
     BigInt bigInt2(stringBigInt2);
+    BigInt result;
 
-    BigInt result = bigInt1 + bigInt2;
-    std::cout << *result.stringArray << "\n";
+    result = bigInt1 + bigInt2;
+    std::cout << *bigInt1.stringArray << " + " << *bigInt2.stringArray << ": " << *result.stringArray << "\n";
 
     result = bigInt1 - bigInt2;
-    std::cout << *result.stringArray << "\n";
-    // std::stoi(*operation)
+    std::cout << *bigInt1.stringArray << " - " << *bigInt2.stringArray << ": " << *result.stringArray << "\n";
 
-    // bigInt1.getResult(*bigInt1.stringArray,*bigInt2.stringArray,operation);
+    ++bigInt1;
+    std::cout << "++" << *bigInt1.stringArray << ": " << bigInt1.num << "\n";
 
     // delete stringBigInt1;
     // delete stringBigInt2;
