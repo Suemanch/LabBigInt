a83a5702c8a3f509d7af1cf9e34307d3316596c4
Sunveray <vlados2016vlados@list.ru>
2025-09-17 11:19:00 +0700

    Update main.cpp
    addition - complete
    substraction - coming

--- main.cpp
+++ main.cpp
@@ -6,24 +6,27 @@
 
 int main()
 {
-    using std::string;
+    auto * operation = new std::string;
 
-    string * operation = new string;
+    std::string stringBigInt1;
+    std::string stringBigInt2;
 
-    string * stringBigInt1 = new string;
-    string * stringBigInt2 = new string;
-
-    std::cin >> *stringBigInt1 >> *operation >> *stringBigInt2;
+    std::cin >> stringBigInt1 >> *operation >> stringBigInt2;
 
     BigInt bigInt1(stringBigInt1);
     BigInt bigInt2(stringBigInt2);
 
+    BigInt result = bigInt1 + bigInt2;
+    std::cout << *result.stringArray << "\n";
+
+    result = bigInt1 - bigInt2;
+    std::cout << *result.stringArray << "\n";
     // std::stoi(*operation)
 
-    bigInt1.getResult(*bigInt1.stringArray,*bigInt2.stringArray,operation);
+    // bigInt1.getResult(*bigInt1.stringArray,*bigInt2.stringArray,operation);
 
-    delete[] stringBigInt1;
-    delete[] stringBigInt2;
+    // delete stringBigInt1;
+    // delete stringBigInt2;
 
     return 0;
 }
