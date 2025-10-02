8251e5e54921486df1bb651c1fcb4ab7f16fc650
Sunveray <vlados2016vlados@list.ru>
2025-10-02 23:15:51 +0700

    cut shit

--- classBigInt.h
+++ classBigInt.h
@@ -9,24 +9,19 @@ private:
     size_t resultLength_ = 0;
     size_t counter_ = 0; // can't be < 0
     int resultInt_ = 0;
-    // bool isMax_ = false;
+    int borrow_;
 
-    // int position_ = 1; // 0 -> 1st num; 1 -> 2nd num
-    int borrow_; // sub
-
+    int deg_; // mul
+    int minus_ = 0;
     int resultDigit_ = 0; // mul
     int countOfZeros_ = 0; // mul
 
     // ------------------------------------------------helpful functions------------------------------------------------
 
-    static char * getMaxStrByLen(char *str1, char *str2);
-    static char * getMinStrByLen(char *str1, char *str2);
-
+    char * insertMinus(char *str);
+    char * deleteMinus(char * str);
     static char * getMaxStr(char* num1, char* num2);
-
     static char * addZeros(char *maxString, char *minString);
-
-    static char * deleteMinus(char * str);
 
     //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -41,15 +36,14 @@ private:
     public:
         size_t length = 0;
         mutable char * stringArray = nullptr;
-        int minus = 0; // if BigInt < 0 --> minus = 1
+        // int minus = 0; // if BigInt < 0 --> minus = 1
 
         explicit BigInt(std::string &input) : length(input.length())
         {
-            // if (input[0] == '-')
-            // {
-            //     minus = 1;
-            //     input.erase(input.begin());
-            // }
+            if (input[0] == '-')
+            {
+                minus_ = 1;
+            }
 
             stringArray = new char[length + 1];
             strcpy(stringArray, input.data());
@@ -63,13 +57,11 @@ private:
             // delete stringArray;
         }
 
-        char * insertMinus(char *str);
-
         BigInt operator+(const BigInt &other);
 
         BigInt operator-(const BigInt &other);
 
-        BigInt& operator++();
+        // BigInt& operator++();
 
         BigInt operator*(const BigInt &other);
 
