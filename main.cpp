81ed542d2c3ddf2b40edde4c65f7bb6b9fadc5ff
Sunveray <vlados2016vlados@list.ru>
2025-10-19 19:36:47 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <string.h>
 #include <string>
 
-#include "classBigInt.h"
+#include "BigInt.h"
 
 int main()
 {
@@ -12,9 +13,55 @@ int main()
 
     std::cin >> stringBigInt1  >> stringBigInt2;
 
+    if (stringBigInt2[0] == '0' || (stringBigInt2[0] == '-' and stringBigInt2[1] == '0'))
+    {
+        std::cout << "incorrect input (division by zero)" << std::endl;
+        return 0;
+    }
+
     BigInt bigInt1(stringBigInt1);
     BigInt bigInt2(stringBigInt2);
     BigInt result;
+
+    bool flag;
+
+    flag = bigInt1 < bigInt2;
+    std::cout << bigInt1.stringArray << " < " << bigInt2.stringArray << ": " << flag << "\n";
+
+    flag = bigInt1 > bigInt2;
+    std::cout << bigInt1.stringArray << " > " << bigInt2.stringArray << ": " << flag << "\n";
+
+    flag = bigInt1 <= bigInt2;
+    std::cout << bigInt1.stringArray << " <= " << bigInt2.stringArray << ": " << flag << "\n";
+
+    flag = bigInt1 == bigInt2;
+    std::cout << bigInt1.stringArray << " == " << bigInt2.stringArray << ": " << flag << "\n";
+
+    flag = bigInt1 >= bigInt2;
+    std::cout << bigInt1.stringArray << " >= " << bigInt2.stringArray << ": " << flag << "\n";
+
+    flag = bigInt1 != bigInt2;
+    std::cout << bigInt1.stringArray << " != " << bigInt2.stringArray << ": " << flag << "\n\n\n";
+
+
+    flag = bigInt1 < 4;
+    std::cout << bigInt1.stringArray << " < " << "4(int)" << ": " << flag << "\n";
+
+    flag = bigInt1 > 100;
+    std::cout << bigInt1.stringArray << " > " << "100(int)" << ": " << flag << "\n";
+
+    flag = bigInt1 <= 34;
+    std::cout << bigInt1.stringArray << " <= " << "34(int)" << ": " << flag << "\n";
+
+    flag = bigInt1 == 5;
+    std::cout << bigInt1.stringArray << " == " << "5(int)" << ": " << flag << "\n";
+
+    flag = bigInt1 >= 8;
+    std::cout << bigInt1.stringArray << " >= " << "8(int)" << ": " << flag << "\n";
+
+    flag = bigInt1 != 12;
+    std::cout << bigInt1.stringArray << " != " << "12(int)" << ": " << flag << "\n\n\n";
+
 
     result = bigInt1 + bigInt2;
     std::cout << stringBigInt1 << " + " << stringBigInt2 << ": " << result.stringArray << "\n";
@@ -28,7 +75,7 @@ int main()
     result = bigInt1 / bigInt2;
     if (strcmp(result.stringArray, "") != 0)
     {
-        std::cout << bigInt1.stringArray << " / " << bigInt2.stringArray << ": " << result.stringArray << "\n\n";
+        std::cout << bigInt1.stringArray << " / " << bigInt2.stringArray << ": " << result.stringArray << "\n\n\n";
     }
 
 
@@ -47,7 +94,8 @@ int main()
 
     std::cout << result.stringArray << " /= ";
     result /= bigInt2;
-    std::cout << bigInt2.stringArray << ": " << result.stringArray << "\n\n";
+    std::cout << bigInt2.stringArray << ": " << result.stringArray << "\n\n\n";
+
 
 
     result = bigInt1 + 125;
@@ -60,7 +108,8 @@ int main()
     std::cout << bigInt1.stringArray << " * " << "4(int)" << ": " << result.stringArray << "\n";
 
     result = bigInt1 / 2;
-    std::cout << bigInt1.stringArray << " / " << "2(int)" << ": " << result.stringArray << "\n\n";
+    std::cout << bigInt1.stringArray << " / " << "2(int)" << ": " << result.stringArray << "\n\n\n";
+
 
 
     result = bigInt1;
@@ -78,7 +127,8 @@ int main()
 
     std::cout << result.stringArray << " /= ";
     result /=6;
-    std::cout << "6(int)" << ": " << result.stringArray << "\n\n";
+    std::cout << "6(int)" << ": " << result.stringArray << "\n\n\n";
+
 
 
     bigInt1 = ++bigInt1;
